a62c83b2cd03cce78c08bdfe8afcfa7da3a4d58c
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-07-02 20:56:13 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').attr('class', 'tasty')
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+function pressIt() {
+  $('body').on('keydown', function(key) {
+    if (key.which === 71) {
+      alert("'g was pressed'")
+    }
+  })
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+  getIt()
+  frameIt()
+  submitIt()
+  pressIt()
 });
